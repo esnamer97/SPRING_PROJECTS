302d116ec31d16f8c122f19971ea8667ee8ee68a
BRAYAN <brayanhernandezmartinez@gmail.com>
2024-05-13 12:40:00 -0500

    GATEWAY_SPRING

---
+++ GATEWAY/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	/*@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://httpbin.org:80"))
+				.build();
+	}*/
+
+	/*@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://httpbin.org:80"))
+				.route(p -> p
+						.host("*.circuitbreaker.com")
+						.filters(f -> f.circuitBreaker(config -> config.setName("mycmd")))
+						.uri("http://httpbin.org:80")).
+				build();
+	}*/
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://httpbin.org:80"))
+				.route(p -> p
+						.host("*.circuitbreaker.com")
+						.filters(f -> f.circuitBreaker(config -> config
+								.setName("mycmd")
+								.setFallbackUri("forward:/fallback")))
+						.uri("http://httpbin.org:80"))
+				.build();
+	}
+
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
+	}
+}
